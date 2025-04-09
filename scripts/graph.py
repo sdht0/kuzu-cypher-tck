b26405785301ba3612402ec8d90abceb3b0fc3cb
Siddhartha Sahu <dev@sdht.in>
2025-04-09 17:00:26 -0400

    Add tracking script

---
+++ scripts/graph.py
@@ -0,0 +1,44 @@
+# This file is part of the uutils coreutils package.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+# From https://github.com/uutils/coreutils-tracking
+
+from datetime import datetime
+
+import sys
+import time
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+if len(sys.argv) <= 1:
+   print('graph.py: <json file>')
+   sys.exit()
+
+d = pd.read_json(sys.argv[1], orient="index")
+df = pd.DataFrame(d)
+
+df.columns.names = ["date"]
+
+as_list = df.index.tolist()
+for i in as_list:
+    idx = as_list.index(i)
+    as_list[idx] = i
+
+df.index = as_list
+
+print(df)
+
+ax = plt.gca()
+df.plot(y="total", color="blue", ax=ax)
+df.plot(y="fail", color="gray", ax=ax, dashes=(2, 1))
+df.plot(y="pass", color="green", ax=ax, dashes=(4, 1))
+if "error" in df:
+   df.plot(y="error", color="orange", ax=ax, dashes=(6, 2))
+df.plot(y="skip", color="violet", ax=ax, dashes=(8, 3))
+plt.title("Kuzu's openCypher testsuite")
+plt.xticks(rotation=45)
+plt.ylim(ymin=0)
+plt.savefig("results.png", dpi=199)
